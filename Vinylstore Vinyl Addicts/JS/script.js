2744678ded74e89fcdce73bda0c271d9cef1a0e3
tiesvanelk <ties.vanelk@edu-kw1c.nl>
2019-02-13 11:40:44 +0100

    ties
    java WIP

--- Vinylstore Vinyl Addicts/JS/script.js
+++ Vinylstore Vinyl Addicts/JS/script.js
@@ -1,2 +1,10 @@
+function submition()
+{
+    var artist = $('#artist').val();
+    var year = $('#year').val();
+    var collect = $('#collectors').val();
 
-document.getElementById('result').innerHTML = 'yeetboii';
+    $('#result').html(artist, year);
+}
+
+$("#send").submit(submition());
