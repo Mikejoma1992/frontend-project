a2d2a231598cf682e7bc4929539b8adca201c707
tiesvanelk <ties.vanelk@edu-kw1c.nl>
2019-02-18 19:36:19 +0100

    Ties
    Gives value to bad or good artist. calculates year with artist value. Doesn't use the checkbox yet
    
    WIP

--- Vinylstore Vinyl Addicts/JS/script.js
+++ Vinylstore Vinyl Addicts/JS/script.js
@@ -10,64 +10,102 @@ function submition()
 $("#checkRarity").submit(function(event)
 {
     var artist = $('#artist').val();
+    var thisYear = new Date().getFullYear();
     var year = $('#year').val();
-    var collect = $('#collectors').val();
+    var checkBoxs = $('#collectors');
+  
     
     var objArtist = [
                         {  
-                            'name': 'Vader Abraham', 
-                            'value': 4
+                            name: 'Vader Abraham', 
+                            value: 4
                         },
                         {
-                            'name': 'BZN',
-                            'value': 4
+                            name: 'BZN',
+                            value: 4
                         },
                         {
-                            'name': 'Kamahl',
-                            'value': 4
+                            name: 'Kamahl',
+                            value: 4
                         },
                         {
-                            'name': 'Heintje',
-                            'value': 4
+                            name: 'Heintje',
+                            value: 4
                         },
                         {
-                            'name': 'Supertramp',
-                            'value': 6
+                            name: 'Supertramp',
+                            value: 6
                         },
                         {
-                            'name': 'Queen',
-                            'value': 6
+                            name: 'Queen',
+                            value: 6
                         },
                         {
-                            'name': 'Metallica',
-                            'value': 6
+                            name: 'Metallica',
+                            value: 6
                         },
                         {
-                            'name': 'Lionel Richie',
-                            'value': 6
+                            name: 'Lionel Richie',
+                            value: 6
                         }];
     var arrayFamousArtist = ['Supertramp', 'Queen', 'Metallica', 'Lionel Richie'];
-
-    for( var i=0; i < arrayBadArtist.length; i++ )
+    var artistValue = 5;
+    var yearCalculate = (thisYear - year) * 0.1;
+    
+    $(document).ready(function () {
+        var ckbox = $('#checkbox');
+        var checkedValue = '';
+        var x = $('#collectors').checked;
+           
+            objArtist.forEach(function(item){
+                if(item.name != artist)
+                {
+                    var notKnownRarity = artistValue + yearCalculate;
+                    $('#result').text(notKnownRarity).show();
+                }
+                else
+                {
+                    objArtist.forEach(function(item){
+                        if(item.name === artist){
+                            
+                            if(item.name === artist)
+                            {
+                                artistValue = item.value;
+                            }
+                        
+                            var rarity = artistValue + yearCalculate;
+                            $('#result').text(artistValue).show();
+                        }
+                    });
+                }
+             });
+        });
+    
+    
+    //$('#result').text(yearCalculate).show();
+    
+    /*for( var i=0; i < objArtist.length; i++ )
     {
-        var rarity = 0;
+       
         
-        if ( artist == arrayBadArtist[i])
+        if ( objArtist.name === artist)
         {
-           rarity = 4;
+            console.log(objArtist.value);
+            
         }
-        else if (artist == arrayFamousArtist[i])
+        else if (artist == objArtist[i])
         {
-            rarity = 6;
+            
             
         }
         else
         {
-            rarity = 5;
+            
+            
         }
+        $('#result').text(objArtist).show();
+       console.log(objArtist);
         
-       console.log(rarity);
-        
-    }   
+    }  */ 
     event.preventDefault();
 });
